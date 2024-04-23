bde40d189bdf4c03d0e1b483486585801221ef3c
EkundayoAlhassan <triplehdayo@gmail.com>
2024-04-23 13:07:58 +0100

    First Commit

---
+++ app/component/hero/hero.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import pic from "../../../public/image/office.jpg"
+
+
+function Hero() {
+  return (
+    <>
+    <section   className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap">
+         <div className="z-10 max-w-2xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl 
+                      dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  
+                      lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          EKUNDAYO FARMS&nbsp;
+          <code className="font-mono font-bold">ekundayofarms.com</code>
+          </p>
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t 
+                          from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                By{" "}
+                <Image
+                  src={pic}
+                  alt="Vercel Logo"
+                  className="dark:invert"
+                  width={100}
+                  height={24}
+                  priority
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default Hero
