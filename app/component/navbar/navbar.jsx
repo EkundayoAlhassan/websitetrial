249be6b768f503dc9160d08f53447cd061b4492b
EkundayoAlhassan <triplehdayo@gmail.com>
2024-04-23 13:23:51 +0100

    new; git commit -m new git commit -m new

--- app/component/navbar/navbar.jsx
+++ app/component/navbar/navbar.jsx
@@ -5,14 +5,13 @@ import pic from "../../../public/image/office.jpg"
 function Navbar() {
   return (
     <>
-    <section   className="text-gray-600 body-font">
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-2xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <header   className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+         <div className="z-10 max-w-2xl w-full items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl 
                       dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  
                       lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           EKUNDAYO FARMS&nbsp;
-          <code className="font-mono font-bold">ekundayofarms.com</code>
           </p>
             <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t 
                           from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -26,9 +25,9 @@ function Navbar() {
                   priority
                 />
             </div>
+          </div>
         </div>
-      </main>
-    </section>
+    </header>
     </>
   );
 }
